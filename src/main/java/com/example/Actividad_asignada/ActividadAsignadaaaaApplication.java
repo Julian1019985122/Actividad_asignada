ede472854d0a714cef8bd90a1cecb22176ebbd5c
Julian1019985122 <Julianby666@gmail.com>
2023-03-28 08:10:03 -0500

    Initial commit

---
+++ src/main/java/com/example/Actividad_asignada/ActividadAsignadaaaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Actividad_asignada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadAsignadaaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadAsignadaaaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Actividad_asignada/ActividadAsignadaaaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Actividad_asignada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadAsignadaaaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
